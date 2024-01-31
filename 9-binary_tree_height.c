51e55b908128e94bcb00b098556428d13254a879
GaboJohn <johngabotsalelwe@gmail.com>
2024-01-31 01:16:35 -0800

    Height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,12 +7,14 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t x, y;
+	if (tree)
+	{
+		size_t x, y;
 
-	if (tree == NULL)
-		return (0);
-	x = binary_tree_height(tree->left);
-	y = binary_tree_height(tree->right);
+		x = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		y =  tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		return (x > y ? x : y);
+	}
 
-	return (1 + (x > y ? x : y));
+	return (0);
 }
